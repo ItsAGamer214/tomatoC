861d2967602ae3a5181ba41e5b3d376c077fb7e4
Anish Reddy Alle <anish.alle11@gmail.com>
2024-07-03 01:38:16 -0500

    messing around

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <ncurses.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{	
+	initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!\n");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+    getch();
+    clear();
+    refresh();
+    printw("you suck, ur gonna kill me :(\n");
+    printw("if you don't press anything, you don't have to kill me!\n");
+    getch();
+    clear();
+    refresh();
+    printw("if you really wanna kill me, press Y\n");
+    char x = getch();
+	endwin();			/* End curses mode		  */
+
+	return 0;
 }
+
+
+
+
+
+
